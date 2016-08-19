d9b1b697c8d7a7ca94f6e19c583b31ed55c7b61f
rafael.cbezerra <rafael.cbezerra@CAS555096W0733.senacsp.edu.br>
2016-08-19 20:25:21 -0300

    -  Primeiro Commit

---
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -0,0 +1,11 @@
+package br.senac.tads3.agenda;
+
+/**
+ *
+ * @author rafael.cbezerra
+ */
+public class Agenda {
+    public static void main(String[] args) {
+        System.out.println("Hello word!!");
+    }
+}
