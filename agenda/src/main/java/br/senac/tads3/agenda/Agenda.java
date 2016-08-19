42b750c8ce5a39c2d712c402f5c104fac193df76
rafael.cbezerra <rafael.cbezerra@CAS555096W0733.senacsp.edu.br>
2016-08-19 20:32:59 -0300

    -  Terceira Alteração

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -6,6 +6,6 @@ package br.senac.tads3.agenda;
  */
 public class Agenda {
     public static void main(String[] args) {
-        System.out.println("Hello word!!");
+        System.out.println("Terceira alteração de mensagem - PI3");
     }
 }
