7a46178bb2ca60f87c30253903457bc7f6efa49b
rafael.cbezerra <rafael.cbezerra@CAS555096W0733.senacsp.edu.br>
2016-08-19 20:28:53 -0300

    -  Primeira Alteração

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -6,6 +6,6 @@ package br.senac.tads3.agenda;
  */
 public class Agenda {
     public static void main(String[] args) {
-        System.out.println("Hello word!!");
+        System.out.println("Primeiro projeto PI3 - prieimeira alteração");
     }
 }
